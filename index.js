e854e9d4113bb537b853e6b55915515542b7c6bc
codewithdpk <codewithdeepak@yahoo.com>
2021-07-03 14:43:57 +0530

    dot env configured.

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require("express");
 
 const app = express();
 
+const dotEnv = require("dotenv");
+
+dotEnv.config();
+
 const drawRoute = require("./routes/Draw");
 
 app.use("/draw", drawRoute);
 
-app.listen(3000, () => {
-  console.log("Server is listening on 4000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is listening on 3000", process.env.PORT);
 });
